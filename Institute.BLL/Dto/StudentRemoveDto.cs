bbabcc23669f87a840b61cc52f7b2a1a7cbd87bd
Trijavico <javier.penaperez08@gmail.com>
2022-11-27 15:21:45 -0400

    Arreglos

--- Institute.BLL/Dto/StudentRemoveDto.cs
+++ Institute.BLL/Dto/StudentRemoveDto.cs
@@ -2,11 +2,8 @@ using System;
 
 namespace Institute.BLL.Dtos
 {
-	public class StudentRemoveDto
+	public class StudentRemoveDto : DtoAudit
 	{
-		public int Id { get; set; }
-		public int UserDeleted { get; set; }
-		public DateTime DeletedDate { get; set; }
 		public bool Deleted { get; set; }
 	}
 }
