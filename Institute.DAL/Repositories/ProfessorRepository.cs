f61096f8ddc72e4508bf9fb114c658f2e3e9e100
Trijavico <javier.penaperez08@gmail.com>
2022-11-28 17:47:55 -0400

    staged

--- Institute.DAL/Repositories/ProfessorRepository.cs
+++ Institute.DAL/Repositories/ProfessorRepository.cs
@@ -17,58 +17,5 @@ namespace Institute.DAL.Repositories
             this.logger = logger;
         }
 
-        public bool Exists(int professorId)
-        {
-            return context.Professors.Any(cd => cd.Id == professorId); 
-        }
-
-        public bool Exist(Expression<Func<Professor, bool>> filter) => context.Professors.Any(filter);
-
-        public IEnumerable<Professor> GetAll()
-        {
-            return context.Professors.OrderByDescending(st => st.CreationDate);
-        }
-
-        public Professor GetProfessor(int professorId)
-        {
-            return context.Professors.Find(professorId);
-        }
-
-        public void Remove(Professor professor)
-        {
-            context.Professors.Remove(professor);
-        }
-
-        public void Save(Professor professor)
-        {
-            context.Professors.Add(professor);
-            context.SaveChanges();
-        }
-
-        public void Update(Professor professor)
-        {
-            try
-            {
-                Professor professorToModify = GetProfessor(professor.Id);
-
-                professorToModify.FirstName = professor.FirstName;
-                professorToModify.LastName = professor.LastName;
-                professorToModify.HireDate = professor.HireDate;
-
-                context.Professors.Update(professorToModify);
-
-                context.SaveChanges();
-            }
-            catch (Exception err)
-            {
-
-                this.logger.LogError($"Error: {err.Message}", err.ToString());
-            }
-        }
-
-        public bool Exists(Expression<Func<Professor, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
